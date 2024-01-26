d620a1cc754fe0ae4f61d1f990508839daecf062
Dan Rios <36534747+riosengineer@users.noreply.github.com>
2024-01-26 14:25:13 +0000

    updates
    pester tests for .tests modules

---
+++ .tests/pester/bicep-module-tests.ps1
@@ -0,0 +1,17 @@
+BeforeDiscovery {
+    # Change the current directory to the Bicep module folder
+    Set-Location -Path "alz\alz_platform\modules"
+    # Define the module and test paths
+    $testPath = "/home/vsts/work/1/s/alz/alz_platform/modules/.tests"
+    $moduleFiles = Get-ChildItem -Filter *.bicep
+    $testFiles = foreach ($moduleFile in $moduleFiles) {
+        Join-Path $testPath ($moduleFile.BaseName + '.tests.bicep')
+    }
+}
+
+Describe "test file" -ForEach $testFiles {
+    # Check if a corresponding .tests.bicep file exists for each .bicep module file
+    It "$_ should exist" {
+        $_ | Should -Exist
+    }
+}
---
+++ .tests/pester/bicep-module-workloads-pester.ps1
@@ -0,0 +1,10 @@
+# create a new container that will be executed
+$container = New-PesterContainer -Path './pipelines/.tests/pester/bicep-module-workloads-tests.ps1'
+
+$config = New-PesterConfiguration
+$config.TestResult.OutputFormat = "NUnitXML"
+$config.TestResult.OutputPath = "test-Pester.xml"
+$config.TestResult.Enabled = $True
+$config.Run.Container = $container
+
+Invoke-Pester -Configuration $config
